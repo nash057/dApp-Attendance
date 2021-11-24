73d14c8851ce551971b89a1dbc75acb290e394a6
Tom Bugert <tombugert@web.de>
2021-11-24 18:08:48 +0100

    add students to app

--- contracts/Classbook.sol
+++ contracts/Classbook.sol
@@ -1,31 +1,48 @@
 pragma solidity ^0.5.0;
 
 contract Classbook {
-  uint public classCount = 0;
+    uint public classCount = 0;
 
-  struct Class {
-    uint id;
-    string name;
-    string teacher;
-  }
+    struct Class {
+        uint id;
+        string name;
+        string teacher;
+        string student1;
+        string student2;
+        string student3;
+        string student4;
+        string student5;
+    }
 
-  /* struct Student {
-    uint id;
-    string name;
-    bool isPresent;
-  } */
+    /* struct Student {
+      uint id;
+      string name;
+      bool isPresent;
+    } */
 
-  event ClassCreated(
-    uint id,
-    string name,
-    string teacher
-  );
+    event ClassCreated(
+        uint id,
+        string name,
+        string teacher,
+        string student1,
+        string student2,
+        string student3,
+        string student4,
+        string student5
+    );
 
-  mapping(uint => Class) public classes;
+    mapping(uint => Class) public classes;
 
-  function createClass(string memory _name, string memory _teacher) public {
-    classCount ++;
-    classes[classCount] = Class(classCount, _name, _teacher);
-    emit ClassCreated(classCount, _name, _teacher);
-  }
+    function createClass(
+        string memory _name,
+        string memory _teacher,
+        string memory _student1,
+        string memory _student2,
+        string memory _student3,
+        string memory _student4,
+        string memory _student5) public {
+        classCount ++;
+        classes[classCount] = Class(classCount, _name, _teacher, _student1, _student2, _student3, _student4, _student5);
+        emit ClassCreated(classCount, _name, _teacher, _student1, _student2, _student3, _student4, _student5);
+    }
 }
--- src/app.js
+++ src/app.js
@@ -48,28 +48,60 @@ App = {
             const schoolClassId = schoolClass[0].toNumber();
             const schoolClassName = schoolClass[1];
             const schoolClassTeacher = schoolClass[2];
+            const schoolClassStudent1 = schoolClass[3];
+            const schoolClassStudent2 = schoolClass[4];
+            const schoolClassStudent3 = schoolClass[5];
+            const schoolClassStudent4 = schoolClass[6];
+            const schoolClassStudent5 = schoolClass[7];
 
-            App.classes.push({classId: schoolClassId, className: schoolClassName, classTeacher: schoolClassTeacher});
+            App.classes.push({
+                classId: schoolClassId,
+                className: schoolClassName,
+                classTeacher: schoolClassTeacher,
+                studentName1: schoolClassStudent1,
+                studentName2: schoolClassStudent2,
+                studentName3: schoolClassStudent3,
+                studentName4: schoolClassStudent4,
+                studentName5: schoolClassStudent5,
+            });
             $('#classes').append(new Option(schoolClassName, schoolClassId));
         }
+        console.log(App.classes)
         $('#classNameLabel').html(App.classes[0].className);
         $('#classTeacherLabel').html(App.classes[0].classTeacher);
+
+        $('#student1Label').html(App.classes[0].studentName1);
+        $('#student2Label').html(App.classes[0].studentName2);
+        $('#student3Label').html(App.classes[0].studentName3);
+        $('#student4Label').html(App.classes[0].studentName4);
+        $('#student5Label').html(App.classes[0].studentName5);
     },
 
     classSelectionChange: async (value) => {
         value--;
         $('#classNameLabel').html(App.classes[value].className);
         $('#classTeacherLabel').html(App.classes[value].classTeacher);
+
+        $('#student1Label').html(App.classes[value].studentName1);
+        $('#student2Label').html(App.classes[value].studentName2);
+        $('#student3Label').html(App.classes[value].studentName3);
+        $('#student4Label').html(App.classes[value].studentName4);
+        $('#student5Label').html(App.classes[value].studentName5);
     },
 
     addClass: async () => {
         const newClass = $('.generateClassForm').serializeArray();
         const className = newClass[0].value;
         const teacherName = newClass[1].value;
-        if (className && teacherName) {
-            console.log(className, teacherName);
+        const student1Name = newClass[2].value;
+        const student2Name = newClass[3].value;
+        const student3Name = newClass[4].value;
+        const student4Name = newClass[5].value;
+        const student5Name = newClass[6].value;
 
-            await App.classbook.createClass(className, teacherName, {from: App.account});
+        if (className && teacherName) {
+            await App.classbook.createClass(className, teacherName,
+                student1Name, student2Name, student3Name, student4Name, student5Name, {from: App.account});
 
             this.location.reload();
         }
