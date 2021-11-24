ab5fca021881c14717f6d22349903299545c4614
Tom Bugert <tombugert@web.de>
2021-11-25 00:41:34 +0100

    add attendance to smart contract

--- contracts/Classbook.sol
+++ contracts/Classbook.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5.0;
 
 contract Classbook {
     uint public classCount = 0;
+    uint public attendanceCount = 0;
 
     struct Class {
         uint id;
@@ -14,11 +15,16 @@ contract Classbook {
         string student5;
     }
 
-    /* struct Student {
-      uint id;
-      string name;
-      bool isPresent;
-    } */
+    struct Attendance {
+        uint id;
+        uint classId;
+        uint256 date;
+        bool student1Attendance;
+        bool student2Attendance;
+        bool student3Attendance;
+        bool student4Attendance;
+        bool student5Attendance;
+    }
 
     event ClassCreated(
         uint id,
@@ -31,7 +37,19 @@ contract Classbook {
         string student5
     );
 
+    event AttendanceCreated(
+        uint id,
+        uint classId,
+        uint256 date,
+        bool student1Attendance,
+        bool student2Attendance,
+        bool student3Attendance,
+        bool student4Attendance,
+        bool student5Attendance
+    );
+
     mapping(uint => Class) public classes;
+    mapping(uint => Attendance) public attendance;
 
     function createClass(
         string memory _name,
@@ -44,5 +62,22 @@ contract Classbook {
         classCount ++;
         classes[classCount] = Class(classCount, _name, _teacher, _student1, _student2, _student3, _student4, _student5);
         emit ClassCreated(classCount, _name, _teacher, _student1, _student2, _student3, _student4, _student5);
+    }
+
+    function createAttendance(
+        uint _classId,
+        uint256 _date,
+        bool _student1Attendance,
+        bool _student2Attendance,
+        bool _student3Attendance,
+        bool _student4Attendance,
+        bool _student5Attendance) public {
+        attendanceCount ++;
+        attendance[attendanceCount] = Attendance(attendanceCount, _classId, _date,
+            _student1Attendance, _student2Attendance, _student3Attendance,
+            _student4Attendance, _student5Attendance);
+        emit AttendanceCreated(attendanceCount, _classId, _date,
+            _student1Attendance, _student2Attendance, _student3Attendance,
+            _student4Attendance, _student5Attendance);
     }
 }
